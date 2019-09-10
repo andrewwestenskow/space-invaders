9c076413f6b1400b4572cbbd978c062f77d8bd6b
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-09-10 11:39:36 -0600

    Enemies move

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const player = document.getElementById('player')
 const game = document.getElementById('game-area')
 const enemies = document.getElementsByClassName('enemy')
-
+const enemiesHold = document.getElementById('enemies-hold')
 const enemyLoc = []
+const pressedKeys = []
+let canShoot = true
+let moveInterval = 1000
+let direction = 'right'
+let lastDirection = ''
+
+//GAME SETUP
 
 const createEnemies = () => {
   let left = 5
   let id = 1
-  for(let i = 0; i < 11; i++){
+  for (let i = 0; i < 11; i++) {
     let row = document.querySelector('.row1')
     let alien = new Image()
     alien.src = './Assets/alien1.png'
@@ -15,35 +22,101 @@ const createEnemies = () => {
     alien.style.left = `${left}%`
     alien.id = `enemy-${id}`
     row.append(alien)
-    left +=8
+    left += 7
     id++
   }
-}
 
-const enemiesMove = () => {
-  
-}
+  for (let i = 0; i < enemies.length; i++) {
+    enemies[i].innerHTML = `<p style='position: absolute; color: white;'>${enemies[i].offsetLeft}</p>`
+    let enemyObj = {
+      id: enemies[i].id,
+      top: enemies[i].offsetTop,
+      left: enemies[i].offsetLeft
+    }
 
+    enemyLoc.push(enemyObj)
+  }
+}
 createEnemies()
 
-for(let i = 0; i < enemies.length; i++){
-  // enemies[i].innerHTML = `<p style='position: absolute; color: white;'>${enemies[i].offsetLeft}</p>`
-  let enemyObj = {
-    id: enemies[i].id,
-    top: enemies[i].offsetTop,
-    left: enemies[i].offsetLeft
-  }
+//ENEMY LOGIC
 
-  enemyLoc.push(enemyObj)
+const handleMove = () => {
+  if (moveInterval > 10) {
+    setTimeout(() => {
+      enemiesMove()
+      handleMove()
+    }, moveInterval);
+  }
 }
 
+handleMove()
 
+const enemiesMove = () => {
+  switch (direction) {
+    case 'right':
+      enemiesRight()
+      break;
+    case 'down':
+      enemiesDown()
+      break
+    case 'left':
+      enemiesLeft()
+    default:
+      return
+  }
+}
 
-const pressedKeys = []
+const enemiesRight = () => {
+  for (let i = 0; i < enemies.length; i++) {
+    let currentLeft = getLeft(enemies[i].style.left)
+    enemies[i].style.left = `${currentLeft + 5}%`
+    if (currentLeft > 85) {
+      direction = 'down'
+      lastDirection = 'right'
+    }
+  }
+}
+
+const enemiesLeft = () => {
+  for (let i = 0; i < enemies.length; i++) {
+    let currentLeft = getLeft(enemies[i].style.left)
+    enemies[i].style.left = `${currentLeft - 5}%`
+    if (currentLeft < 10) {
+      console.log('wall')
+      direction = 'down'
+      lastDirection = 'left'
+    }
+  }
+}
+
+const enemiesDown = () => {
+  for (let i = 0; i < enemies.length; i++) {
+    let currentTop = getLeft(enemies[i].style.top)
+    console.log(currentTop)
+    enemies[i].style.top = `${currentTop + 5}%`
+  }
+  if(lastDirection === 'right'){
+    direction = 'left'
+  } else if(lastDirection === 'left'){
+    direction = 'right'
+  }
+}
+
+//FUNCTIONALITY
 
 const getLeft = (str) => {
   return +str.substr(0, str.length - 1)
 }
+
+const disableShot = () => {
+  canShoot = false
+  setTimeout(() => {
+    canShoot = true
+  }, 500);
+}
+
+//MECHANICS
 
 const handleKeyDown = (e) => {
   switch (e.keyCode) {
@@ -63,7 +136,10 @@ const handleKeyDown = (e) => {
       let index2 = pressedKeys.indexOf(32)
       if (index2 === -1) {
         pressedKeys.push(32)
-        shoot()
+        if (canShoot) {
+          shoot()
+          disableShot()
+        }
       }
       break
   }
@@ -72,11 +148,11 @@ const handleKeyDown = (e) => {
   let rightIndex = pressedKeys.indexOf(39)
   let spaceIndex = pressedKeys.indexOf(32)
 
-  if(leftIndex !== -1) {
+  if (leftIndex !== -1) {
     moveLeft()
   }
 
-  if(rightIndex !== -1){
+  if (rightIndex !== -1) {
     moveRight()
   }
 }
@@ -132,17 +208,17 @@ const shoot = () => {
   const currentLeft = player.style.left
   let laser = document.createElement('div')
   laser.classList.add('laser')
-  laser.style.left = currentLeft
+  laser.style.left = `${getLeft(currentLeft) + 5}%`
   let intervalId = setInterval(() => {
     // laser.innerHTML = `<p style='position: absolute; color: white;'>${laser.offsetLeft}</p>`
     enemyLoc.forEach(element => {
-      if(laser.offsetTop - element.top < 5){
-        if(laser.offsetLeft === element.left || 
-          (laser.offsetLeft - element.left <= 15 && laser.offsetLeft - element.left >= 0)){
+      if (laser.offsetTop - element.top < 5) {
+        if (laser.offsetLeft === element.left ||
+          (laser.offsetLeft - element.left <= 50 && laser.offsetLeft - element.left >= 0)) {
           killEnemy(element, laser)
         }
       }
-      
+
     })
   }, 5);
   laser.addEventListener('animationend', (e) => {
@@ -151,6 +227,9 @@ const shoot = () => {
   })
   game.appendChild(laser)
 }
+
+
+
 
 document.addEventListener('keydown', handleKeyDown)
 document.addEventListener('keyup', handleKeyUp)
